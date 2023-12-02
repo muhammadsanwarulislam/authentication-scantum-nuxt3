b4876b7438cf6281ca975924f5308bc0c5b3d5f6
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-12-02 16:11:13 +0600

    First commit

---
+++ client/pages/login.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { useAuthStore } from "~/stores/useAuthStore";
+import { definePageMeta } from "#imports";
+
+definePageMeta({
+  middleware: ["guest"],
+});
+
+const form = ref({
+  email: "test@example.com",
+  password: "password",
+});
+
+const auth = useAuthStore();
+
+async function handleLogin() {
+  if (auth.isLoggedIn) {
+    return navigateTo("/");
+  }
+
+  const { error } = await auth.login(form.value);
+
+  if (!error.value) {
+    return navigateTo("/");
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleLogin">
+    <label>
+      Email
+      <input v-model="form.email" type="email" />
+    </label>
+    <label>
+      Password
+      <input v-model="form.password" type="password" />
+    </label>
+
+    <button>Login</button>
+  </form>
+</template>
+
+<style scoped></style>
